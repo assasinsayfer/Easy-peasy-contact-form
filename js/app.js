78ce1ba49dc9ec771f677205954a2045a531fee9
Shaxriyor <assasinsayfer@gmail.com>
2024-11-08 20:22:03 +0500

    JavaScript ham yozib bolindi mini loyha tayor

---
+++ js/app.js
@@ -0,0 +1,35 @@
+// !SELECTORS
+
+const fName = document.querySelector("form #fname");
+const age = document.querySelector("form #age");
+const isMarried = document.querySelector("form #isMerried");
+
+const fNameText = document.querySelector(".fName");
+const ageText = document.querySelector(".age");
+const isMarriedText = document.querySelector(".isMerried");
+
+const btnSubmit = document.querySelector(".btnSubmit");
+const btnReset = document.querySelector(".btnReset");
+
+const message = document.querySelector(".message");
+
+// !EVENTS LISTENER & !FUNCTION
+
+fName.addEventListener("input", function (e) {
+  fNameText.innerHTML = `📧 Name: ${e.target.value}`;
+});
+age.addEventListener("input", function (e) {
+  ageText.innerHTML = `🧐 age: ${e.target.value}`;
+});
+isMarried.addEventListener("input", function (e) {
+  isMarriedText.innerHTML = `😘 Is married: ${e.target.value}`;
+});
+
+
+btnSubmit.addEventListener("click", function () {
+  message.innerHTML = "Successful send! ✅";
+});
+
+btnReset.addEventListener("click", function () {
+  message, (innerHTML = "Unsuccessful, try again! 😓");
+});
